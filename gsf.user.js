71fbae0b35f41b0a9b553a5cc4d80d74091b2ced
elekt_riki <6380202@gmail.com>
2026-06-19 10:47:37 +0300

    Update gsf.user.js

--- gsf.user.js
+++ gsf.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           GSF (ИПИГ) - Иконки для писем в Gmail
 // @namespace      https://github.com/npekpacHo/gsf
-// @version        1.82
+// @version        1.83
 // @icon           https://ssl.gstatic.com/ui/v1/icons/mail/rfr/gmail.ico
 // @author         westakof, npekpacHo
 // @description    Добавляет иконки отправителей в Gmail. Оптимизировано для AdGuard.
@@ -167,6 +167,7 @@
   // Значение: имя файла в /images.
   // Пример: https://npekpacho.github.io/gsf/images/platformaofd.png
   const LOCAL_IMAGES = {
+    'chek.pofd.ru': 'platformaofd.png',
     'platformaofd.ru': 'platformaofd.png',
     'cloudpayments.ru': 'cloudpayments.png',
     'aliexpress.ru': 'aliexpress.png',
